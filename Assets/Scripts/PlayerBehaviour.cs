82672d0b74702da32a8bae55bcdcc513662a54f6
Lee Khai <leekhai18@gmail.com>
2018-03-17 18:03:10 +0700

    begin of gameplay

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float verlocityX = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetPosiotion(verlocityX);
+	}
+
+    void SetPosiotion(float x) {
+        transform.position = new Vector3(transform.position.x + verlocityX*Time.deltaTime,  transform.position.y, transform.position.z);   
+    }
+}
